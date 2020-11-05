b0eba5d2b7d93d3b618bfb30fc1a41ea50a5af7a
iv.gonchar <gonchar.ivan@gmail.com>
2020-11-05 19:37:52 +0200

    implement JokesController and thymeleaf template

---
+++ src/main/java/org/gonchar/chucknorrisjokes/controller/JokesController.java
@@ -0,0 +1,24 @@
+package org.gonchar.chucknorrisjokes.controller;
+
+import org.gonchar.chucknorrisjokes.service.JokesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private final JokesService jokesService;
+
+    public JokesController(JokesService jokesService) {
+        this.jokesService = jokesService;
+    }
+
+    @RequestMapping("/")
+    String getJoke(Model model) {
+        String joke = jokesService.getJoke();
+        model.addAttribute("joke", joke);
+        return "index";
+    }
+}
