0475b00197dff5c79ca9ecac9177680f5e35cb1e
iv.gonchar <gonchar.ivan@gmail.com>
2020-11-07 17:39:05 +0200

    refactor import of xml configuration

--- src/main/java/org/gonchar/chucknorrisjokes/App.java
+++ src/main/java/org/gonchar/chucknorrisjokes/App.java
@@ -1,13 +1,22 @@
 package org.gonchar.chucknorrisjokes;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({"classpath*:applicationContext.xml"})
 public class App {
 
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+
+//	@Bean
+//	ChuckNorrisQuotes chuckNorrisQuotes() {
+//		return new ChuckNorrisQuotes();
+//	}
 
 }
