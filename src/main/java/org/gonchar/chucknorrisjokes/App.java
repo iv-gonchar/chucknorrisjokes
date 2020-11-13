6705a3b5c33f5f9f76a59fe0d0376dbe3847514c
iv.gonchar <gonchar.ivan@gmail.com>
2020-11-13 20:53:54 +0200

    remove unused import

--- src/main/java/org/gonchar/chucknorrisjokes/App.java
+++ src/main/java/org/gonchar/chucknorrisjokes/App.java
@@ -4,7 +4,6 @@ import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 public class App {
